a07a3b2bde3cd5f5fc74606b868c9948105b4a76
MS0425 <masking0425@gmail.com>
2020-10-21 21:43:20 +0900

    各モデルのテスト2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe User, type: :model do
       @user = FactoryBot.build(:user)
     end
 
-    # 中略
-
     it "passwordが6文字以上であれば登録できること" do
       @user.password = "123456"
       @user.password_confirmation = "123456"
@@ -26,12 +24,35 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = "1234567"
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    
-      it "重複したemailが存在する場合登録できないこと" do
-        @user.save
-        another_user = FactoryBot.build(:user, email: @user.email)
-        another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
   end
 end
